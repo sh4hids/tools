0828bc2b524ba6d85f483f81fa4e6cbb460af536
Shahidul Islam Majumder <dev@shahid.pro>
2025-11-19 08:40:13 +0600

    feat: add bangla date

--- src/components/BanglaDate.tsx
+++ src/components/BanglaDate.tsx
@@ -5,8 +5,12 @@ export function BanglaDate() {
   const [date, _setDate] = useState(new Date());
 
   return (
-    <span>
+    <a
+      className="dark:text-brand-300 text-brand-700 bg-brand-600/20 rounded-md border border-dashed px-2 py-1"
+      href="https://github.com/sh4hids/bangla-calendar"
+      target="_blank"
+    >
       {getDate(date, { format: 'D MMMM, YYYYb', calculationMethod: 'BD' })}
-    </span>
+    </a>
   );
 }
